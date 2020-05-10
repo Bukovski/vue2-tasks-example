b57324a9d72481af61293cf2ccb1a167e7778d3a
Bukovski <kmanankov@bk.ru>
2020-05-10 14:35:04 +0300

    add: show action log list

--- monster-game/scripts/index.js
+++ monster-game/scripts/index.js
@@ -7,27 +7,36 @@ new Vue({
     actions: []
   },
   methods: {
+    runGame() {
+      this.gameIsRunning = true;
+    },
+    attack() {
+      const playerDamage = this.playerAttack();
+      this.messageTemplate(true, "PLAYER HITS MONSTER FOR " + playerDamage);
+  
+      const monsterDamage = this.monsterAttack();
+      this.messageTemplate(false, "MONSTER HITS PLAYER FOR " + monsterDamage);
+    },
     damageTemplate(min, max) {
       return Math.max(
         Math.floor(Math.random() * max) + 1
         , min);
     },
-    runGame() {
-      this.gameIsRunning = true;
-    },
-    attack() {
-      this.playerAttack();
-      this.monsterAttack();
-    },
     playerAttack() {
       const damage = this.damageTemplate(3, 10);
-      
       this.monsterHealth -= damage;
+      
+      return damage;
     },
     monsterAttack() {
       const damage = this.damageTemplate(5, 12);
-    
       this.playerHealth -= damage;
+      
+      return damage;
+    },
+    messageTemplate(isUser, message) {
+      const id = this.damageTemplate(1, 99999);
+      this.actions = [ { id, isUser, message }, ...this.actions ];
     }
   }
 });
