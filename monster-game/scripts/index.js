ab3e9db247a935be706847708a2dbed2c54cc019
Bukovski <kmanankov@bk.ru>
2020-05-10 13:21:09 +0300

    add: show/hide action buttons

--- monster-game/scripts/index.js
+++ monster-game/scripts/index.js
@@ -2,7 +2,10 @@ new Vue({
   el: "#monster-game",
   data: function () {
     return {
-    
+      playerHealth: 100,
+      monsterHealth: 100,
+      gameIsRunning: false,
+      actions: []
     }
   }
 })
