d435d4a1ef3419792378c13d8b81db46617ca95f
Bukovski <kmanankov@bk.ru>
2020-05-10 14:05:08 +0300

    add: attack action without log

--- monster-game/scripts/index.js
+++ monster-game/scripts/index.js
@@ -1,11 +1,33 @@
 new Vue({
   el: "#monster-game",
-  data: function () {
-    return {
-      playerHealth: 100,
-      monsterHealth: 100,
-      gameIsRunning: false,
-      actions: []
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false,
+    actions: []
+  },
+  methods: {
+    damageTemplate(min, max) {
+      return Math.max(
+        Math.floor(Math.random() * max) + 1
+        , min);
+    },
+    runGame() {
+      this.gameIsRunning = true;
+    },
+    attack() {
+      this.playerAttack();
+      this.monsterAttack();
+    },
+    playerAttack() {
+      const damage = this.damageTemplate(3, 10);
+      
+      this.monsterHealth -= damage;
+    },
+    monsterAttack() {
+      const damage = this.damageTemplate(5, 12);
+    
+      this.playerHealth -= damage;
     }
   }
-})
+});
