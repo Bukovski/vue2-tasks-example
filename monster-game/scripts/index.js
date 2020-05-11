c0e6e2a098a59d96a72ff97f96ed4842be995ba6
Bukovski <kmanankov@bk.ru>
2020-05-11 11:40:00 +0300

    add: check winner

--- monster-game/scripts/index.js
+++ monster-game/scripts/index.js
@@ -8,7 +8,10 @@ new Vue({
   },
   methods: {
     runGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
       this.gameIsRunning = true;
+      this.actions = [];
     },
     attack() {
       const playerDamage = this.damageTemplate(3, 10);
@@ -17,6 +20,8 @@ new Vue({
       this.messageTemplate(true, "PLAYER HITS MONSTER FOR " + playerDamage);
   
       this.monsterAttack();
+  
+      this.checkWinner();
     },
     specialAttack() {
       const playerDamage = this.damageTemplate(10, 20);
@@ -25,6 +30,8 @@ new Vue({
       this.messageTemplate(true, "PLAYER HITS MONSTER HARD FOR " + playerDamage);
   
       this.monsterAttack();
+      
+      this.checkWinner();
     },
     heal() {
       if (this.playerHealth <= 90) {
@@ -36,6 +43,9 @@ new Vue({
   
       this.monsterAttack();
     },
+    giveUp () {
+      this.gameIsRunning = false;
+    },
     damageTemplate(min, max) {
       return Math.max(
         Math.floor(Math.random() * max) + 1
@@ -44,12 +54,29 @@ new Vue({
     monsterAttack() {
       const damage = this.damageTemplate(5, 12);
       this.playerHealth -= damage;
-  
+      
       this.messageTemplate(false, "MONSTER HITS PLAYER FOR " + damage);
     },
     messageTemplate(isUser, message) {
       const id = this.damageTemplate(1, 99999);
       this.actions = [ { id, isUser, message }, ...this.actions ];
+    },
+    checkWinner() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You won! Play again?')) {
+          this.runGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+      }
+  
+      if (this.playerHealth <= 0) {
+        if (confirm('You lose! Play again?')) {
+          this.runGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+      }
     }
   }
 });
